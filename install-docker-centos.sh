ce0e1c30e533e4ffdc89abb7ef769347402356f0
dyoshikawa <dyoshikawa1993@gmail.com>
2018-03-25 17:03:50 +0900

    feat script for centos

---
+++ install-docker-centos.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+yum update -y
+yum remove docker \
+                  docker-common \
+                  docker-selinux \
+                  docker-engine
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-edge
+yum install -y docker-ce
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+systemctl enable docker
+systemctl start docker
